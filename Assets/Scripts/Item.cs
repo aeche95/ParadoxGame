664780f091d345c5e1efbaa74475e24951a1d5e4
Andres <66284241+aeche95@users.noreply.github.com>
2026-02-28 10:07:27 -0500

    examples for audio and lighting

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,11 +5,36 @@ public class Item : Interactable, IStorable
     [SerializeField]
     ItemData itemDataSO;
 
+    AudioSource AudioSource;
+
+    ParticleSystem vfx;
+
     public StorableID ID { get => itemDataSO.ID; }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+        vfx = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public override void Interact(GameObject instigator)
     {
-        throw new System.NotImplementedException();
+        if (AudioSource != null)
+        {
+            AudioSource.Play();
+        }
+
+        if (vfx != null)
+        {
+            vfx.Play();
+        }
     }
 
     public void Remove()
@@ -22,15 +47,5 @@ public class Item : Interactable, IStorable
         throw new System.NotImplementedException();
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
